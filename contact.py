b50cc6ede6f4e58af36199e69c4776a8302d51f2
MilaPetrova <lvptrv83@gmail.com>
2015-08-09 12:13:59 -0400

    создан новый контакт

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__(self, firstname, lastname, email, dayOB, mounthOB, YOB):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email= email
+        self.dayOB = dayOB
+        self.mounthOB = mounthOB
+        self.YOB = YOB
