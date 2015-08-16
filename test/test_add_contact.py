df6048b7ce99f5121e04677b71447b24a1d88852
MilaPetrova <lvptrv83@gmail.com>
2015-08-15 23:34:36 -0400

    созданы пакеты для разных зон ответственности

---
+++ test/test_add_contact.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from model.contact import Contact
+from fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="Alisa", lastname="Petrova", email="alisa.petrova@gmail.com", nickname="Lesi", title="QA", company="Roga i kopita", address="10 Happy St., Boston, MA", home="978-111-1111"))
+    app.logout()
+
