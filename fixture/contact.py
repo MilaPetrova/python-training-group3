3fa32a9a7357d6b4e2de71d1fbafedbf60db87fc
MilaPetrova <lvptrv83@gmail.com>
2015-08-21 23:19:30 -0400

    добавлены проверки в тесты на удаление группы и контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -45,3 +45,7 @@ class ContactHelper:
     def open_contact_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
+
+    def count(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_name("selected[]"))
