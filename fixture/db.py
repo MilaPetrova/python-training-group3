93bf27b9c5d5821aba7b6b4e0dbfb71d5ccb4ff1
MilaPetrova <lvptrv83@gmail.com>
2015-09-17 08:52:19 -0400

    создана фикстура для базы данных

---
+++ fixture/db.py
@@ -0,0 +1,13 @@
+__author__ = 'Liudmila'
+import mysql.connector
+
+class Dbfixture:
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+
+    def destroy(self):
+        self.connection.close()
